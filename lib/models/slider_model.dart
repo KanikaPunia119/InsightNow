c9be265189528385cbe193b1e7cc9ad95ed65067
kanikapunia119 <kanikapunia119@gmail.com>
2023-11-10 23:20:56 +0530

    Commit

---
+++ lib/models/slider_model.dart
@@ -0,0 +1,11 @@
+class sliderModel{
+
+  String? author;
+  String? title;
+  String? description;
+  String? url;
+  String? urlToImage;
+  String? content;
+
+  sliderModel({required this.author, required this.content,required this.description,required this.title,required this.url,required this.urlToImage});
+}
